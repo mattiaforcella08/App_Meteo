50a800d4b03fcee4cba15daaf5c7d67e0e28a32e
Forcella Mattia <mattia.forcella@stu.istitutoturoldo.edu.it>
2026-05-10 12:24:03 +0300

    Iniziato Codice Avvio

--- app/src/main/java/com/example/cloudguard/Avvio.kt
+++ app/src/main/java/com/example/cloudguard/Avvio.kt
@@ -1,12 +1,16 @@
 package com.example.cloudguard
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Avvio : AppCompatActivity() {
+
+    lateinit var Avvio: Button;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,16 @@ class Avvio : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Avvio = findViewById<Button>(R.id.BottoneAvvio);
+
+        Avvio.setOnClickListener {
+
+            // Intent = passaggio da un activity all'altra
+            // val intent = Intent(this, );
+            // startActivity(intent);
+
+        }
+
     }
 }

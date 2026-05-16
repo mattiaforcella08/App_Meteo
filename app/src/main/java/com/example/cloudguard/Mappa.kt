c5330fe4f261aa5dfd350a1e5cdb13844d6fd16f
Forcella Mattia <mattia.forcella@stu.istitutoturoldo.edu.it>
2026-05-16 17:07:35 +0200

    Continuato Test Leaflet

--- app/src/main/java/com/example/cloudguard/Mappa.kt
+++ app/src/main/java/com/example/cloudguard/Mappa.kt
@@ -1,25 +1,55 @@
 package com.example.cloudguard
 
+import android.content.Intent
 import android.os.Bundle
+import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class Mappa : AppCompatActivity() {
+    private lateinit var webView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mappa)
 
-        val webView = findViewById<WebView>(R.id.mapWebView)
+        webView = findViewById(R.id.mapWebView)
 
-        // IMPORTANTE: Abilita JavaScript
+        // Configurazione WebView e attivazione JavaScript
         webView.settings.javaScriptEnabled = true
-
-        // Evita che i link si aprano nel browser esterno
         webView.webViewClient = WebViewClient()
 
-        // Carica la pagina HTML locale dagli assets
+        // Creazione del ponte tra Kotlin e il file HTML (window.Android)
+        webView.addJavascriptInterface(WebAppInterface(), "Android")
+
+        // Caricamento della mappa dagli assets
         webView.loadUrl("file:///android_asset/mappa.html")
+    }
+
+    // Classe interna che riceve i dati inviati dal JavaScript di Leaflet
+    inner class WebAppInterface {
+        @JavascriptInterface
+        fun onMarkerClick(dest: String) {
+            try {
+                // Settiamo la destinazione
+                val destinazione = Class.forName("com.example.cloudguard.$dest")
+
+
+                // Configurazione dell'Intent per aprire la nuova Activity
+                val intent = Intent(
+                    this@Mappa,
+                    destinazione
+                )  //@Mappa serve pk se no kotlin nn sa se this si riferisce a activity o a webInterface
+
+                // Avvio della nuova Activity
+                startActivity(intent)
+            } catch (e:ClassNotFoundException) {
+                // Questo errore esce se hai scritto male il nome nel JS o non hai ancora creato l'Activity in Android Studio
+                Toast.makeText(this@Mappa, "Activity non trovata: $dest", Toast.LENGTH_LONG).show()
+                e.printStackTrace();
+            }
+        }
     }
 }
